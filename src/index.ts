168616abc6ffe18eb6e3d922855d5b2a494b05e2
Vivek <kumarrawanivivek@gmail.com>
2024-07-06 18:49:50 +0530

    .

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,69 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from "@apollo/server/express4"
+import bodyParser from "body-parser";
 
+import dotenv from "dotenv";
+import { authors, books } from "./data"
+import { ApolloServer } from "@apollo/server";
 dotenv.config();
 
-const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send({message : "Express + TypeScript Server"});
-});
+async function initServer(port: number) {
+  const typeDefs = `
+  type Author {
+    name : String
+    books : Int
+    id : String
+    }
+  type Book {
+    title: String
+    authorId: String
+    author : Author
+    }
+  type Query {
+      books: [Book]
+      authors : [Author]
+      getAuthor(authorId : String) : Author
+    }
+      `
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-})
+  const resolvers = {
+    Book: {
+      author: (book: any) => {
+        console.log(book)
+        const res = authors.find((author) => author.id == book.authorId);
+        return res;
+      }
+    },
+    Query: {
+      books: () => books,
+      authors: () => authors,
+      getAuthor: (parent: unknown, { authorId }: any) => {
+        console.log(authorId);
+        // console.log(parent)
+        const res = authors.find((author) => author.id == authorId);
+        console.log(res)
+        return res
+      }
+    },
+  };
+  const server = new ApolloServer({
+
+    typeDefs,
+    resolvers,
+  });
+  await server.start()
+
+  const app: Express = express();
+  app.use(bodyParser.json())
+  app.use("/graphql", expressMiddleware(server))
+
+  app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  })
+
+}
+
+initServer(Number(port));
