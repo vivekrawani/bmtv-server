2128f12cbb8dc6ea0f8e739c55bc3dfcb3a21d16
Vivek <kumarrawanivivek@gmail.com>
2024-07-06 19:14:28 +0530

    add dummy data

---
+++ src/data.ts
@@ -0,0 +1,32 @@
+export const books = [
+    {
+      title: 'The Awakening',
+      authorId: 'KateChopin',
+    },
+    {
+      title: 'City of Glass',
+      authorId: 'PaulAuster',
+    },
+    {
+        title: 'Ice and Fire',
+        authorId: 'PaulAuster',
+      },
+  ];
+
+  export const authors = [
+    {
+        id : 'KateChopin',
+        name : 'Kate Chopin',
+        books : 8,
+    },
+    {
+        id : 'PaulAuster',
+        name : 'Paul Auster',
+        books : 2
+    },
+    {
+        id : 'GeorgerOrwell',
+        name : 'Georger Orwell',
+        books : 5
+    }
+  ]
